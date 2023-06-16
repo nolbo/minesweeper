0b2cb6f7af0809b0a48df557fe592f2c05763ef1
nolbo <jun9975sun@gmail.com>
2023-06-16 16:29:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,66 +5,46 @@ import sys
 import keyboard
 from typing import Final
 
-ground = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
-viewPoint = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
+MAP_WIDTH: Final = 35
+MAP_HEIGHT: Final = 16
+NUM_OF_MINESWEEPER: Final = (MAP_WIDTH * MAP_HEIGHT) // 100 * 20
+
+ground = [[0 for i in range(MAP_WIDTH)] for j in range(MAP_HEIGHT)]
+viewPoint = [[0 for i in range(MAP_WIDTH)] for j in range(MAP_HEIGHT)]
+minesweeperPoint = []
 playCount = 0
 end = False
 startTime = time.time()
-pos = [8, 8] # [x, y]
+pos = [MAP_WIDTH // 2, MAP_HEIGHT // 2] # [x, y]
+
+def numOfMinesweeperAsPercentageOfMapSize(percentage) :
+    return (MAP_WIDTH * MAP_HEIGHT) // 100 * percentage
 
 def printGround(x = -1, y = -1) :
     os.system('cls')
     rowNum = '     '
-    for i in range(9) :
-        rowNum += '\033[35m0\033[0m '
-    for i in range(7) :
-        rowNum += '\033[95m1\033[0m '
+    for i in range(MAP_WIDTH) :
+        if ((i + 1) // 10) % 2 == 0 : 
+            rowNum += '\033[95m{0}\033[0m '.format((i + 1) // 10)
+        else :
+            rowNum += '\033[35m{0}\033[0m '.format((i + 1) // 10)
+    
     rowNum += '\n     '
-    for i in range(9) :
-        rowNum += '\033[35m' + str(i + 1) + '\033[0m '
-    for i in range(7) :
-        rowNum += '\033[95m' + str(i) + '\033[0m '
-    rowNum += '\n     ' + '━' * 31
+    for i in range(MAP_WIDTH) :
+        if ((i + 1) // 10) % 2 == 0 : 
+            rowNum += '\033[95m{0}\033[0m '.format((i + 1) % 10)
+        else :
+            rowNum += '\033[35m{0}\033[0m '.format((i + 1) % 10)
+    rowNum += '\n     ' + '━' * (MAP_WIDTH * 2 - 1)
     print(rowNum)
-
-    for i in range(16) :
-        if (i < 9) : r = '\033[35m0' + str(i + 1) + '\033[0m │ '
-        else : r = '\033[95m' + str(i + 1) + '\033[0m │ '
-        for j in range(16) :
+    
+    for i in range(MAP_HEIGHT) :
+        if ((i + 1) // 10) % 2 == 0 : 
+            r = '\033[95m{0}\033[0m │ '.format(str(i + 1).zfill(2))
+        else :
+            r = '\033[35m{0}\033[0m │ '.format(str(i + 1).zfill(2))
+        
+        for j in range(MAP_WIDTH) :
             if (x != -1 and y != -1 and x == j and y == i)  :
                 r += '\033[1m\033[7m'
             match viewPoint[i][j] :
@@ -105,27 +85,25 @@ def openBlankTile(x, y, _check = []) :
     recursionPos = [[y - 1, x - 1], [y - 1, x], [y - 1, x + 1], [y, x - 1], [y, x + 1], [y + 1, x - 1], [y + 1, x], [y + 1, x + 1]]
 
     for i in recursionPos :
-        if (i[0] > 15 or i[0] < 0) or (i[1] > 15 or i[1] < 0) :
+        if (i[0] > (MAP_HEIGHT - 1) or i[0] < 0) or (i[1] > (MAP_WIDTH - 1) or i[1] < 0) :
             continue
         if i in check :
             continue
         openBlankTile(i[1], i[0], check)
 
 def openAllTile() :
-    _i = 0
-    for i in viewPoint :
-        _j = 0
-        for j in i :
-            viewPoint[_i][_j] = 1
-            _j += 1
-        _i += 1
+    global viewPoint
+    viewPoint = [[1 for i in range(MAP_WIDTH)] for j in range(MAP_HEIGHT)]
+    for [y, x] in minesweeperPoint :
+        viewPoint[y][x] = 2
 
 def isGameEnded() :
-    for i in range(16) :
-        for j in range(16) :
+    for i in range(MAP_HEIGHT) :
+        for j in range(MAP_WIDTH) :
             match viewPoint[i][j] :
                 case 0 :
-                    return False
+                    if ground[i][j] != 'm' : 
+                        return False
                 case 2 :
                     if ground[i][j] != 'm' :
                         return False
@@ -133,11 +111,11 @@ def isGameEnded() :
     end = True
     return True
 
-def createGround(startX, startY, _count_mine = 40) :
+def createGround(startX, startY, _count_mine = NUM_OF_MINESWEEPER) :
     count_mine = _count_mine
-
+    
     while count_mine > 0 :
-        [y, x] = [random.randrange(0, 16), random.randrange(0, 16)]
+        [y, x] = [random.randrange(0, MAP_HEIGHT), random.randrange(0, MAP_WIDTH)]
         if x == startX and y == startY :
             continue
 
@@ -149,16 +127,17 @@ def createGround(startX, startY, _count_mine = 40) :
             ]
             if not [y, x] in recursionPos :
                 ground[y][x] = 'm'
+                minesweeperPoint.append([y, x])
 
-                for i in range(y - 1 if y > 0 else 0, y + 2 if y < 15 else 16) : # y축
-                    for j in range(x - 1 if x > 0 else 0, x + 2 if x < 15 else 16) : # x축
+                for i in range(y - 1 if y > 0 else 0, y + 2 if y < (MAP_HEIGHT - 1) else MAP_HEIGHT) : # y축
+                    for j in range(x - 1 if x > 0 else 0, x + 2 if x < (MAP_WIDTH - 1) else MAP_WIDTH) : # x축
                         if ground[i][j] == 'm' or (startX == j and startY == i) :
                             continue
                         ground[i][j] += 1
                 count_mine -= 1
 
 def printGameInfoMsg(_playCount, _startTime, msg = 'default') :
-    DEFAULT_LENGTH_OF_LINE: Final = 54
+    DEFAULT_LENGTH_OF_LINE: Final = 23 + (MAP_WIDTH * 2 - 1)
     endTime = time.time()
     _playTime = int(endTime - _startTime)
     countMsg = 'Count(s): \033[33m{0}\033[0m'.format(_playCount)
@@ -170,11 +149,11 @@ def printGameInfoMsg(_playCount, _startTime, msg = 'default') :
             print(countMsg, timeMsg.rjust(DEFAULT_LENGTH_OF_LINE - len(countMsg) - 1))
             print('Point: \033[1m\033[36m({0:02d}, {1:02d})\033[0m'.format(pos[0], pos[1]))
         case 'win' :
-            print('=' * 15, '\033[94mWIN!\033[0m', '=' * 15)
+            print('=' * (MAP_WIDTH - 1), '\033[94mWIN!\033[0m', '=' * (MAP_WIDTH - 1))
             print(countMsg, timeMsg.rjust(DEFAULT_LENGTH_OF_LINE - len(countMsg) - 1))
             print('Last: \033[1m\033[36m({0}, {1})\033[0m'.format(pos[0], pos[1]))
         case 'gameover' :
-            print('=' * 13, '\033[90mGameOver\033[0m', '=' * 13)
+            print('=' * (MAP_WIDTH - 3), '\033[90mGameOver\033[0m', '=' * (MAP_WIDTH - 3))
             print(countMsg, timeMsg.rjust(DEFAULT_LENGTH_OF_LINE - len(countMsg) - 1))
             print('Last: \033[1m\033[36m({0}, {1})\033[0m'.format(pos[0], pos[1]))
     if playCount == 0 :
@@ -191,9 +170,9 @@ def movePoint(key) :
             if pos[0] != 1 :
                 pos[0] -= 1
             else :
-                pos[0] = 16
+                pos[0] = MAP_WIDTH
         case 'right' :
-            if pos[0] != 16 :
+            if pos[0] != MAP_WIDTH :
                 pos[0] += 1
             else :
                 pos[0] = 1
@@ -201,28 +180,28 @@ def movePoint(key) :
             if pos[1] != 1 :
                 pos[1] -= 1
             else :
-                pos[1] = 16
+                pos[1] = MAP_HEIGHT
         case 'down' :
-            if pos[1] != 16 :
+            if pos[1] != MAP_HEIGHT :
                 pos[1] += 1
             else :
                 pos[1] = 1
-    # printGround(pos[0] - 1, pos[1] - 1)
-    # printGameInfoMsg(playCount, startTime)
+
     didChangeBoard()
 
 def willChangeBoard() :
     global playCount
     playCount += 1
     if playCount == 1 :
-        createGround(pos[1] - 1, pos[0] - 1)
+        createGround(pos[0] - 1, pos[1] - 1)
 
 def didChangeBoard() :
-    printGround(pos[0] - 1, pos[1] - 1)
-
     if isGameEnded() == True :
+        openAllTile()
+        printGround(pos[0] - 1, pos[1] - 1)
         printGameInfoMsg(playCount, startTime, 'win')
     else :
+        printGround(pos[0] - 1, pos[1] - 1)
         printGameInfoMsg(playCount, startTime)
 
 def openTile() :
