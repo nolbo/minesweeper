93d4aeeacee194751ca4ff4a63c8fc3edb643de5
nolbo <jun9975sun@gmail.com>
2023-06-23 15:58:23 +0900

    변경 사항 없음

--- test.py
+++ test.py
@@ -1,56 +1,11 @@
-from dataclasses import dataclass
-import keyboard
+arr = [
+    [1, 2, 3, 4, 5],
+    [2, 3, 4, 5, 6],
+    [3, 4, 5, 6, 7]
+]
 
-@dataclass
-class Position() :
-    maxY: int
-    maxX: int
-    y: int # y
-    x: int # x
+# for i in range(len(arr) - 1, 1, -1) :
+#     print('\n\r', *arr[i], sep=' ')
 
-    def moveXTo(self, x: int) :
-        self.x = x
-
-    def moveYTo(self, y: int) :
-        self.y = y
-
-    def moveXBy(self, x: int) :
-        to = (self.x + x) % self.maxX
-        self.x = self.maxX if to == 0 else to
-
-    def moveYBy(self, y: int) :
-        to = (self.y + y) % self.maxY
-        self.y = self.maxY if to == 0 else to
-
-pos = Position(16, 16, 8, 8)
-
-def __movePoint(key) :
-    match key :
-        case 'left' :
-            pos.moveXBy(-1)
-        case 'right' :
-            pos.moveXBy(1)
-        case 'up' :
-            pos.moveYBy(-1)
-        case 'down' :
-            pos.moveYBy(1)
-
-    print('X: {0} Y: {1}'.format(pos.x, pos.y))
-
-keyboard.on_press_key('left', (lambda _ : __movePoint('left')))
-keyboard.on_press_key('right', (lambda _ : __movePoint('right')))
-keyboard.on_press_key('up', (lambda _ : __movePoint('up')))
-keyboard.on_press_key('down', (lambda _ : __movePoint('down')))
-
-pos.moveXBy(33)
-print('X: {0} Y: {1}'.format(pos.x, pos.y))
-
-while True :
-    try :
-        key = keyboard.read_key();
-        
-        if key == 'esc' :
-            break
-    except :
-        print('Invalid commend. Retry')
-    
+print('12345', end='\r')
+print('abcde')
